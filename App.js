38690e88035b14b9fd029b2f95116b5ab0cb1dba
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-22 20:39:54 +0800

    img

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Alert, Image, Text, Button, View } from 'react-native';
+import imgSrc from './assets/favicon.png';
 
 export default function App() {
+  const handleBtn = () => Alert.alert('aa', 'asd', [{ text: 'Da' }, { text: 'net' }]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>CoolSheff asdff!</Text>
+      <Button title={'FFFF'} color="red" onPress={handleBtn} />
+      <Button title="SAS" color="red" onPress={handleBtn} />
+      <Image source={imgSrc} style={{ width: 305, height: 159 }} />
       <StatusBar style="auto" />
     </View>
   );
